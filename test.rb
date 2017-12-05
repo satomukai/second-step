818f1cf6dd52245f45aeeef47b88d88583ccf064
satomukai <ksatoshi1026@icloud.com>
2017-12-05 16:51:36 +0900

    renew test.rb

--- test.rb
+++ test.rb
@@ -1,3 +1,8 @@
-for i in 1..15 
-  puts("This is a test.");
+s = ['T','h','i','s',' ','i','s',' ','a',' ','t','e','s','t','.']
+for i in 0..14
+  for j in 0..14
+    c = s[(i+j)%s.length]
+    print c
+  end
+  print"\n"
 end
